945bbc306a1b627f071aad5a8c656382c8b046b8
Christophe Peeters <chrpeeters@gmail.com>
2017-05-27 09:52:20 +0200

    Add unit tests on Beer constructors

---
+++ WolfPack.ILoveBeer.Domain.Tests/BeerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WolfPack.ILoveBeer.Domain.Entities;
+using WolfPack.ILoveBeer.Domain.ValueObjects;
+
+namespace WolfPack.ILoveBeer.Domain.Tests
+{
+    [TestClass]
+    public class BeerTests
+    {
+        [TestMethod]
+        public void ConstructorWithoutId_GeneratesId()
+        {
+            var brand = "Brand";
+            var subBrand = "SubBrand";
+            var beerStyle = BeerStyle.Blond;
+            var beerType = BeerType.Trappist;
+
+            var underTest = new Beer(brand, subBrand, beerStyle, beerType);
+
+            Assert.AreEqual(brand, underTest.Brand);
+            Assert.AreEqual(subBrand, underTest.SubBrand);
+            Assert.AreEqual(beerStyle, underTest.Style);
+            Assert.AreEqual(beerType, underTest.Type);
+            Assert.AreNotEqual(default(Guid), underTest.Id);
+        }
+
+        [TestMethod]
+        public void ConstructorWithId_DoesNotGenerateId()
+        {
+            var brand = "Brand";
+            var subBrand = "SubBrand";
+            var beerStyle = BeerStyle.Blond;
+            var beerType = BeerType.Trappist;
+            var id = Guid.NewGuid();
+
+            var underTest = new Beer(id, brand, subBrand, beerStyle, beerType);
+
+            Assert.AreEqual(brand, underTest.Brand);
+            Assert.AreEqual(subBrand, underTest.SubBrand);
+            Assert.AreEqual(beerStyle, underTest.Style);
+            Assert.AreEqual(beerType, underTest.Type);
+            Assert.AreEqual(id, underTest.Id);
+        }
+
+    }
+}
---
+++ WolfPack.ILoveBeer.Domain.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WolfPack.ILoveBeer.Domain.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WolfPack.ILoveBeer.Domain.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1fe678d5-1b37-4339-a363-f5f17dd3801b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
